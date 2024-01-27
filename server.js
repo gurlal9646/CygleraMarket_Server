618cd9e19084cae71cda60262088a48fdd9f5bd7
Gurlal Singh <gurlal.singh@paygdigitals.com>
2024-01-27 11:18:38 -0500

    Enaled Cors

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const port = process.env.PORT;
 const express = require("express");
 
 const app = express();
+const cors = require('cors');
+
+app.use(cors());
 app.use(express.json());
 
 app.set("view engine", "ejs");
