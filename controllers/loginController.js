b94c9e216845d0419e6ada4f70db7847fff5a800
Gurlal Singh <gurlal.singh@paygdigitals.com>
2024-02-05 13:32:21 -0500

    LOGIN Api changes

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,93 +1,89 @@
-require('dotenv').config();
-const jwt = require('jsonwebtoken');
+require("dotenv").config();
+const jwt = require("jsonwebtoken");
 const { connect } = require("../utils/DataBase.js");
 const { encryptPassword, comparePasswords } = require("../utils/bcrypt.js");
 const { Seller } = require("../utils/models/SellerInfo.js");
-const ApiResponse = require('../utils/models/ApiResponse.js'); // Importing ApiResponse from the apiResponse.js file
+const ApiResponse = require("../utils/models/ApiResponse.js"); // Importing ApiResponse from the apiResponse.js file
 const { Counter } = require("../utils/models/Counter.js");
-const {ResponseCode, ResponseMessage,Roles} = require('../utils/Enums.js'); 
-const { AccessInfo } = require('../utils/models/AccessInfo.js');
+const {
+  ResponseCode,
+  ResponseSubCode,
+  ResponseMessage,
+  Roles,
+} = require("../utils/Enums.js");
+const { AccessInfo } = require("../utils/models/AccessInfo.js");
 
-
-const loginController = async function(request,response){
-    const { email , password,roleId } = request.body;
-    const defaultRoleId = 0;
-    const userRoleId = roleId || defaultRoleId;
-    let userId
-    if (!await AccessInfo.findOne({ email })) {
-        const result = new ApiResponse(ResponseCode.FAILURE, ResponseMessage.NONEXISTINGUSER, ResponseMessage.NONEXISTINGUSERMESSAGE, null);
-        response.json(result);
+const loginController = async function (request, response) {
+  const { email, password, roleId } = request.body;
+  let userInfo;
+  let userId;
+  if (!(await AccessInfo.findOne({ email }))) {
+    const result = new ApiResponse(
+      ResponseCode.FAILURE,
+      ResponseSubCode.USERNOTEXISTS,
+      ResponseMessage.NONEXISTINGUSERMESSAGE,
+      null
+    );
+    response.json(result);
+  } else {
+    if (roleId == 0 || roleId == undefined) {
+      userInfo = await AccessInfo.find({ email });
+      console.log(userInfo);
+      if (userInfo.length > 1) {
+        const result = new ApiResponse(
+          ResponseCode.FAILURE,
+          ResponseSubCode.MULTIPLEACCOUNT,
+          ResponseMessage.MULTIPLEACCOUNT,
+          null
+        );
+        return response.json(result);
+      }
+    } else {
+      userInfo = await AccessInfo.find({ email, roleId });
+      console.log(userInfo);
     }
-    else{
 
-        const userInfo = await AccessInfo.findOne({ email });
-        console.log(await comparePasswords(password,userInfo.password))
-        console.log(userRoleId)
-        console.log(userInfo.roleId)
 
-        if(userRoleId == 0 && (await comparePasswords(password,userInfo.password))){
+    if (await comparePasswords(password, userInfo[0].password)) {
+      const token = jwt.sign(
+        {
+          userId: userInfo.sellerId > 0 ? userInfo.sellerId : userInfo.buyerId,
+          email: userInfo.email,
+          role: userInfo.roleId,
+        },
+        process.env.SECRET_KEY
+      );
+      const result = new ApiResponse(
+        ResponseCode.SUCCESS,
+        ResponseMessage.LOGINUSER,
+        ResponseMessage.LOGINUSERMESSAGE,
+        { token }
+      );
+      response.json(result);
+    } else {
+      console.log("Wrong password");
 
-            if (userInfo.roleId == Roles.SELLER){
-
-                const token = jwt.sign({
-                    userId: userInfo.sellerId,
-                    email: userInfo.email,
-                    role: userInfo.roleId
-                },process.env.SECRET_KEY);
-                userId = userInfo.sellerId;
-                const result = new ApiResponse(ResponseCode.SUCCESS, ResponseMessage.LOGINUSER, ResponseMessage.LOGINUSERMESSAGE, {token});
-                response.json(result);
-            }
-            if (userInfo.roleId == Roles.BUYER){
-                userId = userInfo.buyerId;
-                const result = new ApiResponse(ResponseCode.SUCCESS, ResponseMessage.LOGINUSER, ResponseMessage.LOGINUSERMESSAGE, null);
-                response.json(result);
-            }
-            if (userInfo.roleId == Roles.ADMIN){
-                
-            }
-        }
-        else if(roleId == Roles.BUYER && comparePasswords(password,userInfo.password)){
-            userId = userInfo.buyerId;
-            const result = new ApiResponse(ResponseCode.SUCCESS, ResponseMessage.LOGINUSER, ResponseMessage.LOGINUSERMESSAGE, null);
-            response.json(result);
-    
-
-        }
-        else if(userId == Roles.SELLER && comparePasswords(password,userInfo.password)){
-            userId = userInfo.sellerId;
-            const result = new ApiResponse(ResponseCode.SUCCESS, ResponseMessage.LOGINUSER, ResponseMessage.LOGINUSERMESSAGE, null);
-            response.json(result);
-
-        }
-        else if(userId === Roles.ADMIN && comparePasswords(password,userInfo.password)){
-           
-
-        }
-        else{
-            console.log("Wrong password")
-
-            const result = new ApiResponse(ResponseCode.FAILURE, ResponseMessage.LOGINFAILED, ResponseMessage.WRONGPASSMESSAGE, null);
-            response.json(result);
-        }
-        
-        
-
+      const result = new ApiResponse(
+        ResponseCode.FAILURE,
+        ResponseMessage.LOGINFAILED,
+        ResponseMessage.WRONGPASSMESSAGE,
+        null
+      );
+      response.json(result);
     }
-}
-
+  }
+};
 
 connect()
-    .then((connectedClient) => {
-        client = connectedClient;
+  .then((connectedClient) => {
+    client = connectedClient;
 
-        console.log("Connected to MongoDB Login router");
-    })
-    .catch((err) => {
-        console.error("Failed to connect to MongoDB", err);
+    console.log("Connected to MongoDB Login router");
+  })
+  .catch((err) => {
+    console.error("Failed to connect to MongoDB", err);
 
-        process.exit(1); // Exit the application if the database connection fails
-    });
-
+    process.exit(1); // Exit the application if the database connection fails
+  });
 
 module.exports = { loginController };
--- utils/Enums.js
+++ utils/Enums.js
@@ -10,19 +10,25 @@ class ResponseCode{
     static FAILURE = 0;
 }
 
+class ResponseSubCode{
+    static USERNOTEXISTS =1;
+    static MULTIPLEACCOUNT = 2;
+}
+
 class ResponseMessage{
 
         static EXISTINGUSER = 'USER_ALREADY_EXISTS';
         static EXISTINGUSERMESSAGE = 'User with this email already exists';
         static NEWUSER = 'REGISTERED_SUCCESSFULLY';
         static NEWUSERMESSAGE = 'User added';
-        static NONEXISTINGUSER = 'USER_DOES_NOT_EXISTS';
         static NONEXISTINGUSERMESSAGE = 'User with this email does not exists';
         static LOGINUSER = 'LOGIN_SUCCESSFULL'
         static LOGINUSERMESSAGE ='User logged in.'
         static LOGINFAILED = 'LOGIN_FAILED'
-        static WRONGPASSMESSAGE = 'Invalid Password'
+        static WRONGPASSMESSAGE = 'Invalid Password';
+        static MULTIPLEACCOUNT = 'Multiple account found for the given email';
+
 
 }
 
-module.exports = { Roles ,ResponseCode ,ResponseMessage };
+module.exports = { Roles ,ResponseCode ,ResponseSubCode,ResponseMessage };
