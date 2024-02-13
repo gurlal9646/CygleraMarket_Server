2e25b9e291fbda4c43143cefd508bc8722cb87ef
Gurlal Singh <gurlal.singh@paygdigitals.com>
2024-02-13 17:29:29 -0500

    API refactor

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -11,103 +11,87 @@ const {
   Roles,
 } = require("../utils/Enums.js");
 const { AccessInfo } = require("../utils/models/AccessInfo.js");
-const logger = require('../utils/logger.js');
+const logger = require("../utils/logger.js");
 const { Buyer } = require("../utils/models/BuyerInfo.js");
-
-
 
 const Token = async function (request, response) {
   logger.info(`Login Request: ${JSON.stringify(request.body)}`);
   const { email, password, roleId } = request.body;
-  const tokenResponse  = await generateToken({ email, password, roleId });
+  const tokenResponse = await generateToken({ email, password, roleId });
   response.json(tokenResponse);
-
 };
 
-
-const generateToken = async ({ email, password, roleId })=>{
-  
+const generateToken = async ({ email, password, roleId }) => {
   let accessInfo;
-  if (!(await AccessInfo.findOne({ email }))) {
-    const result = new ApiResponse(
-      ResponseCode.FAILURE,
-      ResponseSubCode.USERNOTEXISTS,
-      ResponseMessage.NONEXISTINGUSERMESSAGE,
-      null
-    );
-    return result;
-  } else {
-    if (roleId == 0 || roleId == undefined) {
-      accessInfo = await AccessInfo.find({ email });
-      if (accessInfo.length > 1) {
-        const result = new ApiResponse(
-          ResponseCode.FAILURE,
-          ResponseSubCode.MULTIPLEACCOUNT,
-          ResponseMessage.MULTIPLEACCOUNT,
-          null
-        );
-        return result;
-      }
+  let result = new ApiResponse(ResponseCode.FAILURE, 0, "", null);
+
+  try {
+    if (!(await AccessInfo.findOne({ email }))) {
+      result.subcode = ResponseSubCode.USERNOTEXISTS;
+      result.message = ResponseMessage.NONEXISTINGUSERMESSAGE;
+      return result;
     } else {
-
-      accessInfo = await AccessInfo.findOne({ email, roleId });
-    }
-
-
-    if (await comparePasswords(password, accessInfo.password)) {
-
-      let userInfo;
-      switch(accessInfo.roleId) {
-        case Roles.SELLER:
-          userInfo = await Seller.findOne({sellerId: accessInfo.sellerId });
-          break;
-
-          case Roles.BUYER:
-            userInfo = await Buyer.findOne({buyerId: accessInfo.buyerId });
-            break;
-
-        // Add other role cases if needed
-        default:
-          break;
+      if (roleId == 0 || roleId == undefined) {
+        accessInfo = await AccessInfo.find({ email });
+        if (accessInfo.length > 1) {
+          result.subcode = ResponseSubCode.MULTIPLEACCOUNT;
+          result.message = ResponseMessage.MULTIPLEACCOUNT;
+          return result;
+        }
+      } else {
+        accessInfo = await AccessInfo.findOne({ email, roleId });
       }
-      const token = jwt.sign(
-        {
-          userId: accessInfo.sellerId > 0 ? accessInfo.sellerId : accessInfo.buyerId,
-          email: accessInfo.email,
-          roleId: accessInfo.roleId,
-        },
-        process.env.SECRET_KEY
-      );
-      const result = new ApiResponse(
-        ResponseCode.SUCCESS,
-        ResponseMessage.LOGINUSER,
-        ResponseMessage.LOGINUSERMESSAGE,
-        {
+      if (await comparePasswords(password, accessInfo.password)) {
+        let userInfo;
+        switch (accessInfo.roleId) {
+          case Roles.SELLER:
+            userInfo = await Seller.findOne({ sellerId: accessInfo.sellerId });
+            break;
+          case Roles.BUYER:
+            userInfo = await Buyer.findOne({ buyerId: accessInfo.buyerId });
+            break;
+          // Add other role cases if needed
+          default:
+            break;
+        }
+
+        const token = jwt.sign(
+          {
+            userId:
+              accessInfo.sellerId > 0
+                ? accessInfo.sellerId
+                : accessInfo.buyerId,
+            email: accessInfo.email,
+            roleId: accessInfo.roleId,
+          },
+          process.env.SECRET_KEY
+        );
+
+        result.code = ResponseCode.SUCCESS;
+        result.message = ResponseMessage.LOGINUSERMESSAGE;
+        result.data = {
           token,
           roleId,
           firstName: userInfo.firstName,
           lastName: userInfo.lastName,
-          email: accessInfo.email
-        }
-      );
-      return result;
-    } else {
-
-      const result = new ApiResponse(
-        ResponseCode.FAILURE,
-        ResponseMessage.LOGINFAILED,
-        ResponseMessage.WRONGPASSMESSAGE,
-        null
-      );
-      return result;
+          email: accessInfo.email,
+        };
+        return result;
+      } else {
+        result.message = ResponseMessage.WRONGPASSMESSAGE;
+        return result;
+      }
     }
+  } catch (error) {
+    logger.error(`Error occurred in generateToken: ${error}`);
+    result.subcode = 100;
   }
+  return result;
 };
 
 connect()
   .then((connectedClient) => {
     client = connectedClient;
-
   })
   .catch((err) => {
     console.error("Failed to connect to MongoDB", err);
@@ -115,4 +99,4 @@ connect()
     process.exit(1); // Exit the application if the database connection fails
   });
 
-module.exports = { Token,generateToken };
+module.exports = { Token, generateToken };
