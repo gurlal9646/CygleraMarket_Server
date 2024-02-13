9555f08523a8c0c3716c404777725f135076c647
Gurlal Singh <gurlal.singh@paygdigitals.com>
2024-02-13 17:48:18 -0500

    Logs format changed

--- utils/logger.js
+++ utils/logger.js
@@ -1,15 +1,33 @@
 const { createLogger, format, transports } = require("winston");
+const { v4: uuidv4 } = require('uuid');
+
+// Define a custom format to include the traceid
+const customFormat = format.printf(({ level, message, timestamp, traceid }) => {
+    return `${timestamp} | ${traceid} | [${level.toUpperCase()}] | ${message}`;
+});
+
+// Function to generate a traceid
+const generateTraceId = () => {
+    return uuidv4();
+};
 
 const logger = createLogger({
     transports: [
         new transports.Console()
     ],
-    format:  format.combine(
+    format: format.combine(
         format.timestamp(),
-        format.json(),
-        format.metadata(),
-        format.prettyPrint()
+        format.metadata({ fillExcept: ['timestamp', 'level', 'message', 'label', 'traceid'] }), 
+        format.prettyPrint(),
+        format.splat(), 
+        format(function dynamicMeta(info, opts) {
+            if (!info.traceid) {
+                info.traceid = generateTraceId();
+            }
+            return info;
+        })(),
+        customFormat
     )
-})
+});
 
-module.exports = logger
+module.exports = logger;
