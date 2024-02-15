5008509a7ac7127a6087db907244e5d1f454f3ff
Gurlal Singh <gurlal.singh@paygdigitals.com>
2024-02-14 19:59:52 -0500

    Validation for product schema

--- utils/models/Product.js
+++ utils/models/Product.js
@@ -3,36 +3,50 @@ const Schema = mongoose.Schema;
 
 const productSchema = new Schema({
     productId: Number,
-    sellerId:Number,
-    name: String,
-    description:String,
-    price:Number,
-    category:String,
-    manufacturer:String,
-    stockQuantity:Number,
+    sellerId: Number,
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true,
+        min: 1
+    },
+    category: String,
+    manufacturer: String,
+    stockQuantity: {
+        type: Number,
+        required: true,
+        min: 1
+    },
     expiryDate: {
         type: Date,
-        default: null, // Set default value to null
+        default: null // Set default value to null
     },
     storageConditions: String,
     createdAt: {
         type: Date,
-        default: Date.now, // Set default value to the current date
-      },
-      createdBy: {
+        default: Date.now // Set default value to the current date
+    },
+    createdBy: {
         type: Number,
-        default: null,
-      },
-      updatedAt: {
+        default: null
+    },
+    updatedAt: {
         type: Date,
-        default: null, // Set default value to null
-      },
-      updatedBy: {
+        default: null // Set default value to null
+    },
+    updatedBy: {
         type: Number,
-        default: null, // Set default value to null
-      },
-  });
-  
-const Product = mongoose.model('product', productSchema,'Product');
+        default: null // Set default value to null
+    }
+});
 
-module.exports = {Product} ;
+const Product = mongoose.model('product', productSchema, 'Product');
+
+module.exports = { Product };
