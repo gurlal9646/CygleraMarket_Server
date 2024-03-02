763606ea63ea779d8acc3140d90924febf06734c
Gurlal Singh <gurlal.singh@paygdigitals.com>
2024-03-02 11:19:26 -0500

    API fixes

--- utils/models/AccessInfo.js
+++ utils/models/AccessInfo.js
@@ -1,58 +1,56 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const accessInfoSchema = new Schema({
-    accessInfoId: {
-        type:String,
-        required: true
-    },
-    email: {
-        type: String,
-        required: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    isLocked: {
-        type: Boolean,
-        default: false,
-        required: true
-    },
-    buyerId: {
-        type: Number,
-        default: 0,
-        required: true
-    },
-    sellerId: {
-        type: Number,
-        default: 0,
-        required: true
-    },
-    roleId: {
-        type: Number,
-        default: 0,
-        required: true
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now,
-        required: true
-    },
-    createdBy: {
-        type: Number,
-        default: null,
-    },
-    updatedAt: {
-        type: Date,
-        default: null,
-    },
-    updatedBy: {
-        type: Number,
-        default: null,
-    }
+  accessInfoId: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  isLocked: {
+    type: Boolean,
+    default: false,
+    required: true,
+  },
+  buyerId: {
+    type: String,
+    default: "",
+  },
+  sellerId: {
+    type: String,
+    default: "",
+  },
+  roleId: {
+    type: Number,
+    default: 0,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    required: true,
+  },
+  createdBy: {
+    type: Number,
+    default: null,
+  },
+  updatedAt: {
+    type: Date,
+    default: null,
+  },
+  updatedBy: {
+    type: Number,
+    default: null,
+  },
 });
 
-const AccessInfo = mongoose.model('accessInfo', accessInfoSchema, 'AccessInfo');
+const AccessInfo = mongoose.model("accessInfo", accessInfoSchema, "AccessInfo");
 
 module.exports = { AccessInfo };
